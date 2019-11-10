2bdd55246c3e5cfd0873c8211752c2e69b84fa13
Илья Крутько <mr.darkilya@mail.ru>
2019-11-10 04:43:49 +0300

    Переход к фрагментам, добавлен drawer, пофикшены баги

--- app/src/main/java/ru/team_grade/grade_mobile/MainActivity.java
+++ app/src/main/java/ru/team_grade/grade_mobile/MainActivity.java
@@ -1,14 +1,19 @@
 package ru.team_grade.grade_mobile;
 
 import android.os.Bundle;
-import android.webkit.CookieManager;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.Menu;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-//FIXME Main activity должен открываться только 1 раз, вне зависимости, сколько раз нажата кнопка
-//FIXME Если нажать "Назад", то Login activity открываться не должен
 //TODO Не показывать экран логина при сохраненных данных
 //TODO Реализовать парсер и красиво все оформить
 //TODO Сохранять данные в бд, чтобы просматривать информацию оффлайн
@@ -16,16 +21,34 @@ import androidx.appcompat.app.AppCompatActivity;
 //TODO Добавить расписание
 
 public class MainActivity extends AppCompatActivity {
-    WebView webView;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        webView = findViewById(R.id.webView);
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setCookie("openid.sfedu.ru", "openid_server=" + User.token);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://grade.sfedu.ru/handler/sign/openidlogin?loginopenid=" + User.login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_main).setDrawerLayout(drawer).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
